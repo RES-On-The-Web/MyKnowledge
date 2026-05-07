837e7ad082d464bf99d4097fce9c3822cf0b76af
RES-On-The-Web <radesorkh@gmail.com>
2026-05-07 19:27:17 +0330

    chapters 14, 15 and 16 are done. The end of this journey

---
+++ 004-Python-tutorial-chapters-docs.python.org/010_[learningArea] 14_ Interactive Input Editing and History Substitution & 15_ Floating-Point Arithmetic & 16_ Appendix.py
@@ -0,0 +1,15 @@
+    # 14. Interactive Input Editing and History Substitution
+# 
+# # some extra information
+
+
+    # 15. Floating-Point Arithmetic: Issues and Limitations
+# 
+# '0.1' is not 0.1 , it's 0.1000000000000000055511151231257827021181583404541015625
+# # more accurate calculations = 'from decimal import Decimal' and 'from fractions import Fraction'
+
+
+    # 16. Appendix
+# 
+# use '#!/usr/bin/env python3' and 'chmod +x myscript.py' to run .py
+# and more information about usercustomize (user) and sitecustomize (root)
