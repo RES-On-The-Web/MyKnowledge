2c6cc7a5c30d1f528dcbb918f1532c6a204a2b21
RES-On-The-Web <radesorkh@gmail.com>
2026-06-27 20:18:50 +0330

    to do list project -done

--- 009-JavaScript-learn-topics-www.youtube.com-freeCodeCamp.org/003_[learningArea] to do list/script.js
+++ 009-JavaScript-learn-topics-www.youtube.com-freeCodeCamp.org/003_[learningArea] to do list/script.js
@@ -1,9 +1,26 @@
-let todoList = []
+let todoList = [
+    {
+        name: 'a1sdfsdfasdf',
+        dueDate: '123412342134'
+    },
+    {
+        name: 'a2sdfasdfasdf',
+        dueDate: '345234534'
+    },
+    {
+        name: 'a3sdfasdf43253qdfsgsdfg',
+        dueDate: '34532453245'
+    }]
 
 function addTodo(){
     const inputNameElement = document.querySelector(".js-name-input")
-    todoList.push(inputNameElement.value)
+    const inputDueDateElement = document.querySelector(".js-due-date-input")
+    todoList.push({
+        name: inputNameElement.value,
+        dueDate: inputDueDateElement.value
+    })
     inputNameElement.value = ''
+    inputDueDateElement.value = ''
     console.log(todoList)
     listGenerator()
 }
@@ -11,9 +28,24 @@ function addTodo(){
 function listGenerator(){
     let listItems = ''
     for(let i = 0; i < todoList.length; i++){
-        listItems += `<p>${todoList[i]}</p>`
+        listItems += `
+        <div>${todoList[i].name}</div>
+        <div>${todoList[i].dueDate}</div>
+        <button onclick="
+            todoList.splice(${i},1);
+            listGenerator();
+        " class="normal-button delete-button">Delete</button>`
     }
     listBody = document.querySelector(".list-body")
     listBody.innerHTML = listItems
 }
 listGenerator()
+
+
+
+
+// //******************************************************** */
+
+
+const [a,b,c] = [1,2,3]
+console.log(a,b,c);
